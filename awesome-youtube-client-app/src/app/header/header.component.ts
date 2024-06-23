789698e38e399c23c2e37a11eb14039948d64c5a
lyutails <lyusya.cool@gmail.com>
2024-06-23 03:50:03 +0300

    feat: add input field to header

--- awesome-youtube-client-app/src/app/header/header.component.ts
+++ awesome-youtube-client-app/src/app/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatIconModule } from '@angular/material/icon';
+import { SearchInputFieldComponent } from '../search-input-field/search-input-field.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatSlideToggleModule, MatIconModule],
+  imports: [MatSlideToggleModule, MatIconModule, SearchInputFieldComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
