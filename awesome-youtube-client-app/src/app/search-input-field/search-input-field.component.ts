4511a421835728ba0e10bb353d9161973c5994c3
lyutails <lyusya.cool@gmail.com>
2024-06-23 12:52:24 +0300

    feat: add couple of material icons and custom theme

--- awesome-youtube-client-app/src/app/search-input-field/search-input-field.component.ts
+++ awesome-youtube-client-app/src/app/search-input-field/search-input-field.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-search-input-field',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './search-input-field.component.html',
   styleUrl: './search-input-field.component.scss'
 })
