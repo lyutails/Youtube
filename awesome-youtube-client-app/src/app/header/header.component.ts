cb3f3c487f30e019da7be0a715ce853386cd5e54
lyutails <lyusya.cool@gmail.com>
2024-06-23 03:41:47 +0300

    feat: add material icon for login

--- awesome-youtube-client-app/src/app/header/header.component.ts
+++ awesome-youtube-client-app/src/app/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatSlideToggleModule],
+  imports: [MatSlideToggleModule, MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
---
+++ awesome-youtube-client-app/src/app/search-input-field/search-input-field.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-input-field',
+  standalone: true,
+  imports: [],
+  templateUrl: './search-input-field.component.html',
+  styleUrl: './search-input-field.component.scss'
+})
+export class SearchInputFieldComponent {
+
+}
