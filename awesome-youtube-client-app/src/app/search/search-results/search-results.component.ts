711e00ebbeb49a2447f8b2a341127c79e89f88db
lyutails <lyusya.cool@gmail.com>
2024-06-24 17:53:33 +0300

    feat: work on adding data to cards

--- awesome-youtube-client-app/src/app/search/search-results/search-results.component.ts
+++ awesome-youtube-client-app/src/app/search/search-results/search-results.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import * as cards from '../../../../response.json';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-search-results',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './search-results.component.html',
   styleUrl: './search-results.component.scss',
 })
