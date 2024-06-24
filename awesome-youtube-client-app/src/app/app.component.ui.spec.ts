8dfaf8e7cba97ae618eb5617d5129a5bedb5c723
lyutails <lyusya.cool@gmail.com>
2024-06-24 05:51:33 +0300

    feat: add one more similar test to ui spec to check

--- awesome-youtube-client-app/src/app/app.component.ui.spec.ts
+++ awesome-youtube-client-app/src/app/app.component.ui.spec.ts
@@ -2,8 +2,13 @@ import { render, screen } from '@testing-library/angular';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-    it('should render YouTube-client-app title', async () => {
-        await render(AppComponent);
-        await screen.getByText('YouTube-client-app');
-    })
-})
+  it('should render YouTube-client-app title', async () => {
+    await render(AppComponent);
+    await screen.getByText('YouTube-client-app');
+  });
+
+  it('should render Settings', async () => {
+    await render(AppComponent);
+    await screen.getByText('Settings');
+  });
+});
