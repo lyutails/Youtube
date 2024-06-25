14ec8185c8301ca4cb157bc835f473411b679f33
lyutails <lyusya.cool@gmail.com>
2024-06-25 15:47:46 +0300

    feat: try out mat button

--- awesome-youtube-client-app/src/app/filters/filters.component.ts
+++ awesome-youtube-client-app/src/app/filters/filters.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInput } from '@angular/material/input';
 
 @Component({
   selector: 'app-filters',
   standalone: true,
-  imports: [],
+  imports: [MatButton, MatIconModule, MatInput],
   templateUrl: './filters.component.html',
   styleUrl: './filters.component.scss'
 })
