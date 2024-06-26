e36f22c9c404e9e0306823341b365dec934f83a7
lyutails <lyusya.cool@gmail.com>
2024-06-26 16:16:54 +0300

    feat: hearts adding removing by one

--- awesome-youtube-client-app/src/app/search/search-item/search-item.component.ts
+++ awesome-youtube-client-app/src/app/search/search-item/search-item.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { SearchItem } from '../search-item.model';
+import { CommonModule } from '@angular/common';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-search-item',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatIcon],
   templateUrl: './search-item.component.html',
-  styleUrl: './search-item.component.scss'
+  styleUrl: './search-item.component.scss',
 })
 export class SearchItemComponent {
+  @Input() card!: SearchItem;
+  protected isFavourite = false;
 
+  countFavourites() {
+    /* const heart = event?.currentTarget as HTMLButtonElement;
+    this.responseCards.map((item) => {
+      if (item.id === heart.id) {
+        this.favourites.push(item.id);
+        this.isFavourite = true;
+      }
+    });*/
+
+    this.isFavourite = !this.isFavourite;
+  }
 }
--- awesome-youtube-client-app/src/app/search/search-results/search-results.component.ts
+++ awesome-youtube-client-app/src/app/search/search-results/search-results.component.ts
@@ -2,20 +2,15 @@ import { Component } from '@angular/core';
 import * as cards from '../../../../response.json';
 import { CommonModule } from '@angular/common';
 import { MatIcon } from '@angular/material/icon';
+import { SearchItemComponent } from '../search-item/search-item.component';
 
 @Component({
   selector: 'app-search-results',
   standalone: true,
-  imports: [CommonModule, MatIcon],
+  imports: [CommonModule, MatIcon, SearchItemComponent],
   templateUrl: './search-results.component.html',
   styleUrl: './search-results.component.scss',
 })
 export class SearchResultsComponent {
   responseCards = cards.items;
-
-  isFavourite = false;
-
-  loveUnlove() {
-    this.isFavourite = !this.isFavourite;
-  }
 }
