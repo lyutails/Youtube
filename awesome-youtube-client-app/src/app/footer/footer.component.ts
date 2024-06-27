3d18f592822591eda023eef7f5b94506391bb9fe
lyutails <lyusya.cool@gmail.com>
2024-06-27 18:53:17 +0300

    feat: change theme changing button to footer

--- awesome-youtube-client-app/src/app/footer/footer.component.ts
+++ awesome-youtube-client-app/src/app/footer/footer.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatIcon],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
   author = '@lyutails';
+  isDay = true;
+  isLight = 'wb_sunny';
+  isDark = 'bedtime';
+
+  changeTheme() {
+    this.isDay = !this.isDay;
+  }
 }
