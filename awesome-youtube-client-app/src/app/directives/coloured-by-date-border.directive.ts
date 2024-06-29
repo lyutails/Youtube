a6878b089a10b291ea41a654406deafcf338b896
lyutails <lyusya.cool@gmail.com>
2024-06-30 00:21:49 +0300

    feat: add coloured by date bottom borders to cards

--- awesome-youtube-client-app/src/app/directives/coloured-by-date-border.directive.ts
+++ awesome-youtube-client-app/src/app/directives/coloured-by-date-border.directive.ts
@@ -8,70 +8,38 @@ export class ColouredByDateBorderDirective implements OnInit {
   today = new Date();
   @Input() appColouredByDateBorder = '';
 
-  ngOnInit(): void {
-    if (
-      this.today.getMonth() -
-        new Date(this.appColouredByDateBorder).getMonth() ===
-      0
-    ) {
-      console.log('lalala1');
-      console.log(this.appColouredByDateBorder);
-      this.elem.nativeElement.style.borderBottom = '10px solid red';
-    } else {
-      console.log('lalala');
-      console.log(this.appColouredByDateBorder);
-      this.elem.nativeElement.style.borderBottom = '10px solid green';
-    }
+  constructor(public elem: ElementRef) {}
+
+  getDifferenceInTime(dateOne: Date, dateTwo: string) {
+    return Math.round(
+      (dateOne.getTime() - new Date(dateTwo).getTime()) / (1000 * 3600 * 24)
+    );
   }
 
-  /* const redBorderSixMonthsAgo = new Date().setMonth(
-    new Date().getMonth() - 6
-  );
-
-  const yellowBorderOneMonthAgo = new Date().setMonth(
-    new Date().getMonth() - 1
-  ); */
-
-  /* const greenBorderSevenDaysToOneMonthAgo = new Date().setMonth(
-    new Date().getMonth()
-  );
-
-  const blueBorderIfNewerThanSevenDays = new Date().setMonth(
-    new Date().getMonth() - 6
-  ); */
-
-  /* const check =
-    (this.today.getFullYear() -
-      new Date('2024-06-11T12:42:19.000Z').getFullYear()) *
-    12; */
-
-  /* console.log(this.responseCards.map(item => item.snippet.publishedAt));
-  console.log(this.today);
-  console.log(new Date('2024-06-11T12:42:19.000Z'));
-  console.log(
-    (this.today.getFullYear() -
-      new Date('2018-12-01T15:00:05.000Z').getFullYear()) *
-      12
-  );
-  console.log(this.today.getFullYear());
-  console.log(this.today.getMonth());
-  console.log(new Date('2024-05-30T17:46:58.000Z').getMonth()); */
-
-  /* this.responseCards.map(item => {
+  ngOnInit(): void {
     if (
-      this.today.getMonth() -
-        new Date(item.snippet.publishedAt).getMonth() ===
-      0
+      this.getDifferenceInTime(this.today, this.appColouredByDateBorder) >= 180
     ) {
-      console.log('lalala1');
-      this.elem.nativeElement.style.borderBottom = '10px solid green';
-    }
-    else {
-      console.log('lalala');
       this.elem.nativeElement.style.borderBottom = '10px solid red';
     }
-  }); */
-
-  constructor(public elem: ElementRef) {
+    if (
+      this.getDifferenceInTime(this.today, this.appColouredByDateBorder) >= 30 &&
+      this.getDifferenceInTime(this.today, this.appColouredByDateBorder) <= 179
+    ) {
+      this.elem.nativeElement.style.borderBottom = '10px solid yellow';
+    }
+    if (
+      this.getDifferenceInTime(this.today, this.appColouredByDateBorder) >= 7 &&
+      this.getDifferenceInTime(this.today, this.appColouredByDateBorder) <= 29
+    ) {
+      this.elem.nativeElement.style.borderBottom = '10px solid green';
+    }
+    if (
+      this.getDifferenceInTime(this.today, this.appColouredByDateBorder) > 0 &&
+      this.getDifferenceInTime(this.today, this.appColouredByDateBorder) <=
+        6
+    ) {
+      this.elem.nativeElement.style.borderBottom = '10px solid blue';
+    }
   }
 }
