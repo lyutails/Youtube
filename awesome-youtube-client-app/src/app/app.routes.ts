f7c5a645c17f102db66e34b2f6f052587fb91a9e
lyutails <lyusya.cool@gmail.com>
2024-07-02 09:41:16 +0300

    feat: add new pages components and create base routes

--- awesome-youtube-client-app/src/app/app.routes.ts
+++ awesome-youtube-client-app/src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { CardDetailsComponent } from './card-details/card-details.component';
 import { SearchResultsComponent } from './search/search-results/search-results.component';
+import { LoginComponent } from './login/login.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
   /* { component: HomePage, path: '', data: {animation: 'HomePage'} } */
-  { path: '', component: SearchResultsComponent, title: 'main page' },
-  { path: '', component: CardDetailsComponent, title: 'card page' },
+  { path: '', component: SearchResultsComponent, title: 'main' },
+  { path: '', component: LoginComponent, title: 'login' },
+  { path: '', component: CardDetailsComponent, title: 'card' },
+  { path: '', component: NotFoundComponent, title: '404' }
 ];
