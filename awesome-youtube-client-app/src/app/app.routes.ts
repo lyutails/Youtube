3c2a2d44017275de5bb90434e227d9f932adc5e8
lyutails <lyusya.cool@gmail.com>
2024-07-03 02:45:57 +0300

    feat: change some more roots to load component

--- awesome-youtube-client-app/src/app/app.routes.ts
+++ awesome-youtube-client-app/src/app/app.routes.ts
@@ -1,17 +1,29 @@
 import { Routes } from '@angular/router';
 import { CardDetailsComponent } from './youtube/card-details/card-details.component';
-import { NotFoundComponent } from './core/not-found/not-found.component';
-import { SearchResultsComponent } from './youtube/search/search-results/search-results.component';
 
 export const routes: Routes = [
   {
     path: 'login',
     title: 'login',
-    loadComponent: () => import('./auth/login/login.component').then(m => m.LoginComponent)
+    loadComponent: () =>
+      import('./auth/login/login.component').then(m => m.LoginComponent),
   },
-  //{ component: LoginComponent, path: 'login', title: 'login' },
-  { component: SearchResultsComponent, path: 'main', title: 'main' },
+  {
+    path: 'main',
+    title: 'main',
+    loadComponent: () =>
+      import('./youtube/search/search-results/search-results.component').then(
+        m => m.SearchResultsComponent
+      ),
+  },
   { component: CardDetailsComponent, path: 'card/:id', title: 'card' },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { component: NotFoundComponent, path: '**', title: '404' },
+  {
+    path: '**',
+    title: '404',
+    loadComponent: () =>
+      import('./core/not-found/not-found.component').then(
+        m => m.NotFoundComponent
+      ),
+  },
 ];
