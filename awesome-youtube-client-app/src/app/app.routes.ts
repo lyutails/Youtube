e436c2a44632fae556a46aaea5ca4aa7b41ddeae
lyutails <lyusya.cool@gmail.com>
2024-07-03 02:41:13 +0300

    feat: try to implement standalone lazy loading

--- awesome-youtube-client-app/src/app/app.routes.ts
+++ awesome-youtube-client-app/src/app/app.routes.ts
@@ -1,13 +1,17 @@
 import { Routes } from '@angular/router';
 import { CardDetailsComponent } from './youtube/card-details/card-details.component';
-import { LoginComponent } from './auth/login/login.component';
 import { NotFoundComponent } from './core/not-found/not-found.component';
 import { SearchResultsComponent } from './youtube/search/search-results/search-results.component';
 
 export const routes: Routes = [
-  { component: LoginComponent, path: 'login', title: 'login' },
+  {
+    path: 'login',
+    title: 'login',
+    loadComponent: () => import('./auth/login/login.component').then(m => m.LoginComponent)
+  },
+  //{ component: LoginComponent, path: 'login', title: 'login' },
   { component: SearchResultsComponent, path: 'main', title: 'main' },
   { component: CardDetailsComponent, path: 'card/:id', title: 'card' },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { component: NotFoundComponent, path: '**', title: '404' }
+  { component: NotFoundComponent, path: '**', title: '404' },
 ];
