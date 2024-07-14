a5831603d83139e878e5370bd743f9ba1f44c8f5
lyutails <lyusya.cool@gmail.com>
2024-07-14 21:17:59 +0300

    feat: install ngrx store

--- awesome-youtube-client-app/src/app/app.config.ts
+++ awesome-youtube-client-app/src/app/app.config.ts
@@ -9,14 +9,14 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { shortenUrlInterceptor } from './interceptors/shorten-url.interceptor';
 import { loadingSpinnerInterceptor } from './interceptors/loading-spinner.interceptor';
+import { provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withPreloading(PreloadAllModules)),
     provideAnimationsAsync(),
-    provideHttpClient(
-      withInterceptors([shortenUrlInterceptor, loadingSpinnerInterceptor])
-    ),
-  ],
+    provideHttpClient(withInterceptors([shortenUrlInterceptor, loadingSpinnerInterceptor])),
+    provideStore()
+],
 };
